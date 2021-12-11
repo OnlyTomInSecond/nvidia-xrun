9bbea79a36bee9e6604555e482cfdfb2442ef570
OnlyTomInSecond <2718273965@qq.com>
2021-12-11 09:26:22 +0800

    Update install.sh, add module blacklist

--- install.sh
+++ install.sh
@@ -13,26 +13,43 @@ if [ -z "${NVIDIA_PCI_BUS_ID}" ];then
 	echo "Found nvidia card"
 	if [ -z "${NVIDIA_AUDIO_PCI_ID}" ];then
 		echo "Found nvidia audio device"
+	fi
 else
 	echo "Error,Can;t find nvidia card! Exiting"
 	exit 0
 fi
-}
 
+}
+function find_acpi_support(){
 if [ ! -f /sys/bus/pci/devices/0000:${NVIDIA_PCI_BUS_ID}/remove ] ||
    [ ! -f /sys/bus/pci/devices/0000:${NVIDIA_AUDIO_PCI_ID}/remove ];then
 	echo "Can't find /sys/bus/pci/devices/${NVIDIA_PCI_BUS_ID}/remove ! Please check the driver installtion or ACPI module installed and loaded rightly!"
-	exit 0
+	return 0;
+else 
+	echo "Find acpi support"
+	return 1;
+fi
+}
+
+get_info
+
+if [ find_acpi_support == 1 ];then
+	echo "Now copy the files"
+	sudo cp ./nvidia-xrun /usr/bin/
+	sudo cp ./nvidia-xinitrc /etc/X11/xinit
+	sudo mkdir /etc/X11/nvidia-xorg.conf.d
+	sudo cp ./nvidia-xrun-pm.service /etc/systemd/system/
+	sudo cp ./config/nvidia-xrun /etc/default/
+	sudo cp ./modules/nvidia-xrun_blacklist.conf /etc/modprobe.d/
+
+	echo "update systemd-daemon"
+	sudo systemctl deamon-reload
+	echo "enable nvidia-xrun-pm.service"
+	sudo systemctl enable nvidia-xrun-pm.service
+	echo "Update initramfs"
+	sudo mkinitcpio -P
+	echo "Done"
+else
+	echo "Failed!"
 fi
 
-echo "Now copy the files"
-sudo cp ./nvidia-xrun /usr/bin/
-sudo cp ./nvidia-xinitrc /etc/X11/xinit
-mkdir /etc/X11/nvidia-xorg.conf.d
-sudo cp ./nvidia-xrun-pm.service /etc/systemd/system/
-sudo cp ./config/nvidia-xrun /etc/default/
-
-echo "update systemd-daemon"
-sudo systemctl deamon-reload
-echo "enable nvidia-xrun-pm.service"
-sudo systemctl enable nvidia-xrun-pm.service
